6d554cb8d518fcdb6b4b1685eed8a054626b514f
Len Wanger <len_wanger@hotmail.com>
2016-02-15 20:31:34 -0600

    converted to class, added context manager, automatically track # of triangles.

--- pystl.py
+++ pystl.py
@@ -1,121 +1,146 @@
 """
 PySTL - a simple package to write STL files
 
-TODO:
-
-- Only supports ASCII STL files for now. Could add binary files
-- Make it a class and support context manager (so can do with stl_file as stl:
+Len Wanger
+last updated: 02-15-2016
 """
 
 import math
 import struct
 
+class PySTL(object):
+    def __init__(self, file_name, bin=True, model_name='', num_triangles=0):
+        self.f = None
+        self.model_name = model_name
+        self.file_name = file_name
+        self.is_bin = bin
+        self.num_triangles = num_triangles if num_triangles >= 0 else 0
+        self.header_written = self.trailer_written = False
+        self.track_triangles = False if num_triangles > 0 else True
+        self.num_tri_pos = None
 
-def write_stl_header(f, name='', bin=True, num_triangles=0):
-    if bin:
-        header_str = ''
-        f.write(struct.pack("80sI", header_str, num_triangles))
-    else:
-        f.write('solid ' + name + '\n' )
+
+    def open(self):
+        file_mode = 'wb' if self.is_bin else 'w'
+        self.f = open(self.file_name, file_mode)
 
 
-def write_stl_trailer(f, bin=True):
-    if bin:
-        # No trailer on binary STL files
-        pass
-    else:
-        f.write('endsolid \n' )
+    def close(self):
+        self.f.close()
+        self.f = None
 
-def write_stl_triangle(f, triangle, normal=None, bin=True):
-    if bin:
-        """ Each triangle is 4 sets of 3 floats - normal, three verticices, then a byte count (which can be used for color)"""
+
+    def __enter__(self):
+        self.open()
+        self.write_stl_header()
+        return self
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if not self.trailer_written:
+            self.write_stl_trailer()
+        self.close()
+
+
+    def write_stl_header(self):
+        if self.is_bin:
+            header_str = ''
+            self.f.write(struct.pack("80s", header_str))
+            self.write_num_triangles_bin()
+        else:
+            self.f.write('solid ' + self.model_name + '\n' )
+
+
+    def write_num_triangles_bin(self):
+        if self.is_bin:
+            if not self.num_tri_pos:
+                # on __enter__ record the position
+                self.num_tri_pos = self.f.tell()
+            else:
+                # on __exit__ update the number of triangles
+                self.f.seek(self.num_tri_pos)
+            self.f.write(struct.pack("I", 0 if self.num_triangles < 0 else self.num_triangles))
+        else:
+            raise RuntimeError('Cannot call write_num_triangles_bin on a text STL file.')
+
+
+    def write_stl_trailer(self):
+        if self.is_bin:
+            if self.track_triangles:
+                self.write_num_triangles_bin()
+        else:
+            # No trailer on binary STL files
+            self.f.write('endsolid \n' )
+
+
+    def write_stl_triangle(self, triangle, normal=None):
         if not normal:
-            normal = calc_normal(triangle)
-        data = [ normal[0], normal[1], normal[2],
-                 triangle[0][0], triangle[0][1], triangle[0][2],
-                 triangle[1][0], triangle[1][1], triangle[1][2],
-                 triangle[2][0], triangle[2][1], triangle[2][2],
-                 0 ]
-        f.write(struct.pack("12fH", *data))
-    else:
-        if not normal:
-            normal = calc_normal(triangle)
+            normal = self.calc_normal(triangle)
 
-        f.write('  facet normal {:.3f} {:.3f} {:.3f}\n'.format(normal[0], normal[1], normal[2]) )
-        f.write('    outer loop\n' )
-        f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[0][0], triangle[0][1], triangle[0][2]))
-        f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[1][0], triangle[1][1], triangle[1][2]))
-        f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[2][0], triangle[2][1], triangle[2][2]))
-        f.write('    endloop\n' )
-        f.write('  endfacet \n')
+        if self.is_bin:
+            """ Each triangle is 4 sets of 3 floats - normal, three verticices, then a byte count (which can be used for color)"""
 
+            data = [ normal[0], normal[1], normal[2],
+                     triangle[0][0], triangle[0][1], triangle[0][2],
+                     triangle[1][0], triangle[1][1], triangle[1][2],
+                     triangle[2][0], triangle[2][1], triangle[2][2],
+                     0 ]
+            self.f.write(struct.pack("12fH", *data))
+        else:
+            self.f.write('  facet normal {:.3f} {:.3f} {:.3f}\n'.format(normal[0], normal[1], normal[2]) )
+            self.f.write('    outer loop\n' )
+            self.f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[0][0], triangle[0][1], triangle[0][2]))
+            self.f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[1][0], triangle[1][1], triangle[1][2]))
+            self.f.write('      vertex {:.3f} {:.3f} {:.3f}\n'.format(triangle[2][0], triangle[2][1], triangle[2][2]))
+            self.f.write('    endloop\n' )
+            self.f.write('  endfacet \n')
 
-# def write_stl_bin_header(f, num_triangles):
-#     """ Write the header for a binary STL file. The 1st line is 80 characters (usually blank), the 2nd line is a UINT32
-#     for the number of triangles.    """
-#     header_str = ''
-#     f.write(struct.pack("80sI", header_str, num_triangles))
+        if self.track_triangles:
+            self.num_triangles += 1
 
 
-# def write_stl_bin_triangle(f, triangle, normal=None):
-#     """ Each triangle is 4 sets of 3 floats - normal, three verticices, then a byte count (which can be used for color)"""
-#     if not normal:
-#         normal = calc_normal(triangle)
-#     data = [ normal[0], normal[1], normal[2],
-#              triangle[0][0], triangle[0][1], triangle[0][2],
-#              triangle[1][0], triangle[1][1], triangle[1][2],
-#              triangle[2][0], triangle[2][1], triangle[2][2],
-#              0 ]
-#     f.write(struct.pack("12fH", *data))
+    def length_vector(self, v):
+        """ Return the length of a vector """
+        return math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
 
 
-def length_vector(v):
-    """ Return the length of a vector """
-    return math.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    def unit_vector(self, v):
+        """ return the unit vector for a vector """
+        l = self.length_vector(v)
+        return (v[0] / l, v[1] / l, v[2] / l)
 
 
-def unit_vector(v):
-    """ return the unit vector for a vector """
-    l = length_vector(v)
-    return (v[0] / l, v[1] / l, v[2] / l)
+    def calc_normal(self, t):
+        """ Return the normal for a triangle. Make sure it's a unit vector """
+        # U = pt2 - pt1
+        # V = pt3 - pt1
+        # nx = UyVz - UzVy
+        # ny = UzVx - UxVz
+        # nz = UxVy - UyVx
+        u = (t[1][0] - t[0][0], t[1][1] - t[0][1], t[1][2] - t[0][2])
+        v = (t[2][0] - t[0][0], t[2][1] - t[0][1], t[2][2] - t[0][2])
 
-    # if l > 0.00001:
-    #     return (v[0] / l, v[1] / l, v[2] / l)
-    # else:
-    #     print('Got zero length vector!')   # ???
-    #     raise RuntimeError
-    #     return (0.0, 0.0, 0.0)
-
-
-def calc_normal(t):
-    """ Return the normal for a triangle. Make sure it's a unit vector """
-    # U = pt2 - pt1
-    # V = pt3 - pt1
-    # nx = UyVz - UzVy
-    # ny = UzVx - UxVz
-    # nz = UxVy - UyVx
-    u = (t[1][0] - t[0][0], t[1][1] - t[0][1], t[1][2] - t[0][2])
-    v = (t[2][0] - t[0][0], t[2][1] - t[0][1], t[2][2] - t[0][2])
-
-    nx = u[1]*v[2] - u[2]*v[1]
-    ny = u[2]*v[0] - u[0]*v[2]
-    nz = u[0]*v[1] - u[1]*v[0]
-    return unit_vector((nx, ny, nz))
+        nx = u[1]*v[2] - u[2]*v[1]
+        ny = u[2]*v[0] - u[0]*v[2]
+        nz = u[0]*v[1] - u[1]*v[0]
+        return self.unit_vector((nx, ny, nz))
 
 
 if __name__ == '__main__':
     stl_name = 'bin_stl_test.stl'
-    num_triangles=2
+    #num_triangles=2    # pass number of triangles in if you know the number in advance
+    num_triangles=0 # automatically track number of triangles
     v1 = (0.0, 0.0, 0.5)
     v2 = (0.0, 1.0, 0.0)
     v3 = (1.0, 1.0, 0.5)
     v4 = (1.0, 0.0, 0.0)
     t1 = (v1,v2,v4)
     t2 = (v2,v3,v4)
-    with open(stl_name, 'wb') as f:
-        # write_stl_bin_header(f, num_triangles)
-        # write_stl_bin_triangle(f, t1, normal=None)
-        # write_stl_bin_triangle(f, t2, normal=None)
-        write_stl_header(f, bin=True, num_triangles=num_triangles)
-        write_stl_triangle(f, t1, normal=None, bin=True)
-        write_stl_triangle(f, t2, normal=None, bin=True)
+
+    with PySTL('stl_test_bin.stl',  bin=True, num_triangles=num_triangles) as stl:
+        stl.write_stl_triangle(t1)
+        stl.write_stl_triangle(t2)
+
+    with PySTL('stl_test_txt.stl',  bin=False, num_triangles=num_triangles) as stl:
+        stl.write_stl_triangle(t1)
+        stl.write_stl_triangle(t2)
